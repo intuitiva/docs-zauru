abf1db34ec7c42d56d0eafc1d254abda2b0279b4
Lester Guerra <bitbucket@brickapps.com>
2018-11-19 15:00:35 -0600

    display all links on section body

--- src/templates/tutorial.js
+++ src/templates/tutorial.js
@@ -12,6 +12,15 @@ export default class TutorialTemplate extends React.Component {
     const childs_tutorial = props.data.allChildTutorial.edges.map(ct => ct.node);
     const parents_tutorial = props.data.allParentTutorial.edges.map(ct => ct.node);
     const current_parent_id = tutorial.isParent ? tutorial.id : (tutorial.parentTutorial ? tutorial.parentTutorial.id : 0);
+    const childs_tutorial_column_1 = childs_tutorial.filter((tc, index) => tc.parentTutorial.id === current_parent_id).filter((tc, index) => index % 3 === 0);
+    const childs_tutorial_column_2 = childs_tutorial.filter((tc, index) => tc.parentTutorial.id === current_parent_id).filter((tc, index) => index % 3 === 1);
+    const childs_tutorial_column_3 = childs_tutorial.filter((tc, index) => tc.parentTutorial.id === current_parent_id).filter((tc, index) => index % 3 === 2);
+
+    console.log(childs_tutorial);
+    console.log(childs_tutorial_column_1);
+    console.log(childs_tutorial_column_2);
+    console.log(childs_tutorial_column_3);
+
     return (
       <Layout title={tutorial.title} search={true}>
           <div id="left" className="column">
@@ -32,10 +41,10 @@ export default class TutorialTemplate extends React.Component {
                               childs_tutorial.map((tc, key) => (
                                   current_parent_id === tc.parentTutorial.id && 
                                   <Link key={key} to={tc.slug} className={["panel-block", "child", slug === tc.slug ? "is-active" : "" ].join(' ')}>
-                                  &nbsp;&nbsp;&nbsp;&nbsp;
-                                  <span className="panel-icon">
-                                    <FaFileAlt />
-                                  </span>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;
+                                    <span className="panel-icon">
+                                      <FaFileAlt />
+                                    </span>
                                     {tc.title}
                                   </Link>
                               ))
@@ -49,8 +58,47 @@ export default class TutorialTemplate extends React.Component {
           <div id="right" className="column">
             <div className="bottom">
               <div className="content" dangerouslySetInnerHTML={{ __html: tutorial.text.format.html }} ></div>
+              
+              {
+                tutorial.isParent === true &&
+                  childs_tutorial_column_1.map((tc, key) => (
+                    
+                    <div className="tile is-ancestor">
+                      {
+                        childs_tutorial_column_1[key] &&
+                        <div className="tile is-parent is-4">
+                          <article className="tile is-child box">
+                            <Link to={childs_tutorial_column_1[key].slug}>
+                              {childs_tutorial_column_1[key].title}
+                            </Link>
+                          </article>
+                        </div>
+                      }
+                      {
+                        childs_tutorial_column_2[key] &&
+                        <div className="tile is-parent is-4">
+                          <article className="tile is-child box">
+                            <Link to={childs_tutorial_column_2[key].slug}>
+                              {childs_tutorial_column_2[key].title}
+                            </Link>
+                          </article>
+                        </div>
+                      }
+                      {
+                        childs_tutorial_column_3[key] &&
+                        <div className="tile is-parent is-4">
+                          <article className="tile is-child box">
+                            <Link to={childs_tutorial_column_3[key].slug}>
+                              {childs_tutorial_column_3[key].title}
+                            </Link>
+                          </article>
+                        </div>
+                      }
+                    </div>
+                  ))
+              }
+              
             </div>
-            
           </div>
       </Layout>
     )
