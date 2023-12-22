bcd7ab7c94ae02c177b7d2a593159aca1cba3cda
Oscar Rene Cuellar Mancilla <oscar@zauru.com>
2023-12-22 12:20:50 -0600

    # Formats

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,38 +1,38 @@
 const path = require("path");
 const _ = require("lodash");
 
-exports.createPages = ({graphql, actions}) => {
-    const {createPage} = actions
-    return new Promise((resolve, reject) => {
-        const tutorialTemplate = path.resolve('src/templates/tutorial.js')
-        const sectionTemplate = path.resolve('src/templates/section.js')
-        resolve(
-            graphql(`
-                {
-                    allContentfulTutorial {
-                        edges {
-                            node {
-                                id
-                                slug
-                            }
-                        }
-                    }
-                }
-            `).then((result) => {
-                if (result.errors) {
-                    reject(result.errors)
-                }
-                result.data.allContentfulTutorial.edges.forEach((edge) => {
-                    createPage ({
-                        path: edge.node.slug,
-                        component: tutorialTemplate,
-                        context: {
-                            slug: edge.node.slug
-                        }
-                    })
-                })
-                return
-            })
-        )
-    })
-}
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  return new Promise((resolve, reject) => {
+    const tutorialTemplate = path.resolve("src/templates/tutorial.js");
+    const sectionTemplate = path.resolve("src/templates/section.js");
+    resolve(
+      graphql(`
+        {
+          allContentfulTutorial {
+            edges {
+              node {
+                id
+                slug
+              }
+            }
+          }
+        }
+      `).then((result) => {
+        if (result.errors) {
+          reject(result.errors);
+        }
+        result.data.allContentfulTutorial.edges.forEach((edge) => {
+          createPage({
+            path: edge.node.slug,
+            component: tutorialTemplate,
+            context: {
+              slug: edge.node.slug,
+            },
+          });
+        });
+        return;
+      })
+    );
+  });
+};
