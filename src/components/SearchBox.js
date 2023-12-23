759676d31007263a6d4c39ad50705315b36a8f87
Oscar Rene Cuellar Mancilla <oscar@zauru.com>
2023-12-22 18:23:19 -0600

    # Fix searchBox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,35 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 
-class SearchBox extends React.Component {
-  componentDidMount() {
+const SearchBox = () => {
+  useEffect(() => {
     if (window.docsearch) {
       window.docsearch({
         apiKey: "88244b9a5ba892c3bf06c13abd966203",
         indexName: "zauru",
         inputSelector: "#nav-search",
-        debug: true, // Set debug to true if you want to inspect the dropdown,
+        debug: true,
       });
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <div className="control has-icons-left">
-        <input
-          className="input is-rounded docs-nav-search-input"
-          type="text"
-          placeholder="Buscar"
-          id="nav-search"
-          autoComplete="off"
-          style={{ width: `100% !important` }}
-        />
-        <span className="icon is-left">
-          <FaSearch />
-        </span>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="control has-icons-left">
+      <input
+        className="input is-rounded docs-nav-search-input"
+        type="text"
+        placeholder="Buscar"
+        id="nav-search"
+        autoComplete="off"
+        style={{ width: `100% !important` }}
+      />
+      <span className="icon is-left">
+        <FaSearch />
+      </span>
+    </div>
+  );
+};
 
 export default SearchBox;
