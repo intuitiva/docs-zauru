3da21d303d65b288c451e32718c8096ea57f8ae5
Oscar Rene Cuellar Mancilla <oscar@zauru.com>
2024-01-01 23:45:01 -0600

    Update SwaggerComponent.js

--- src/components/SwaggerComponent.js
+++ src/components/SwaggerComponent.js
@@ -1,5 +1,7 @@
 import React from "react";
 import loadable from "@loadable/component";
+import "swagger-ui-react/swagger-ui.css";
+import "../components/css/swagger-custom.css";
 
 const SwaggerUI = loadable(() => import("swagger-ui-react"), {
   fallback: <div>Loading...</div>,
