25d65b7f960a14feb61febd28abbac832b9f8d2b
Oscar Rene Cuellar Mancilla <oscar@zauru.com>
2024-01-01 23:26:26 -0600

    # Fix styles

--- src/components/SwaggerComponent.js
+++ src/components/SwaggerComponent.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
+import "../components/css/swagger-custom.css";
 
 const SwaggerComponent = () => {
   const [isSSR, setIsSSR] = useState(true);
