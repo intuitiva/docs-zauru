e930753764831fdbf51433616659ed115103e1bb
Oscar Rene Cuellar Mancilla <oscar@zauru.com>
2024-01-01 23:38:09 -0600

    Update SwaggerComponent.js

--- src/components/SwaggerComponent.js
+++ src/components/SwaggerComponent.js
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from "react";
-import SwaggerUI from "swagger-ui-react";
-import "swagger-ui-react/swagger-ui.css";
-import "../components/css/swagger-custom.css";
 
 const SwaggerComponent = () => {
-  const [isSSR, setIsSSR] = useState(true);
+  const [isBrowser, setIsBrowser] = useState(false);
+  let SwaggerUI;
 
   useEffect(() => {
-    setIsSSR(typeof window === "undefined");
+    if (typeof window !== "undefined") {
+      SwaggerUI = require("swagger-ui-react");
+      setIsBrowser(true);
+    }
   }, []);
 
-  if (isSSR) {
+  if (!isBrowser) {
     return null;
   }
 
-  return <SwaggerUI url="/swagger.yaml" />;
+  return <SwaggerUI.default url="/swagger.yaml" />;
 };
 
 export default SwaggerComponent;
